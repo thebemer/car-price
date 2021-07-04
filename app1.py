91ccc1d0d979c21447ca4b21a037f7b6999eaf22
Ankit Gupta <75660562+thebemer@users.noreply.github.com>
2021-07-04 18:30:53 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -59,6 +59,9 @@ app.add_url_rule('/tool', 'webio_view', webio_view(predict),
             methods=['GET', 'POST', 'OPTIONS'])
 
 
-app.run(host='localhost', port=80, debug=True)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
 
-#visit http://localhost/tool to open the PyWebIO application.
+    start_server(predict, port=args.port)
